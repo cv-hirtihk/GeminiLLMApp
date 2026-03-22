f15a66fe1196e47aa07ac45a859d275db937a1d3
cv-hirtihk <cvhirtihk@gmail.com>
2026-03-22 18:42:27 +0530

    Initial commit

---
+++ qachat.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+load_dotenv() ## loading all env variables
+
+import streamlit as st
+import os
+from PIL import Image
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## funtion to load Gemini Pro model and get responses
+model = genai.GenerativeModel("gemini-2.5-flash")
+chat = model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response = chat.send_message(question, stream=True)
+    return response
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini LLM Application")
+
+# Initialize session state for chat history if it doesn't exist 
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+input = st.text_input("Input: ", key="input")
+submit = st.button("Ask the question")
+
+if submit and input:
+    response = get_gemini_response(input)
+    # Add user query and response to session chat history
+    st.session_state['chat_history'].append(("You", input))
+    st.subheader("The Response is")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("LLM", chunk.text))
+
+st.subheader("The chat history is")
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
